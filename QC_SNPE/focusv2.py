56e5703242ff92701b1dc7d68747b37cd6da80af
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-06-21 12:33:28 +0800

    Update focusv2.py

--- QC_SNPE/focusv2.py
+++ QC_SNPE/focusv2.py
@@ -2,18 +2,57 @@
 import torch
 import torch.nn as nn
 
+def get_activation(name="silu", inplace=True):
+    if name == "silu":
+        module = nn.SiLU(inplace=inplace)
+    elif name == "relu":
+        module = nn.ReLU(inplace=inplace)
+    elif name == "lrelu":
+        module = nn.LeakyReLU(0.1, inplace=inplace)
+    else:
+        raise AttributeError("Unsupported act type: {}".format(name))
+    return module
+
+class BaseConv(nn.Module):
+    """A Conv2d -> Batchnorm -> silu/leaky relu block"""
+
+    def __init__(
+        self, in_channels, out_channels, ksize, stride, groups=1, bias=False, act="silu"
+    ):
+        super().__init__()
+        # same padding
+        pad = (ksize - 1) // 2
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=ksize,
+            stride=stride,
+            padding=pad,
+            groups=groups,
+            bias=bias,
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = get_activation(act, inplace=True)
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+
 class FocusV2(nn.Module):
-    def __init__(self):
+    """Focus width and height information into channel space."""
+    def __init__(self, in_channels, out_channels, ksize=1, stride=1, act="silu"):
         super(FocusV2, self).__init__()
-        weights = torch.zeros(12, 3, 2, 2)
+        weights = torch.zeros(in_channels * 4, in_channels, 2, 2)
         for i in range(3):
             weights[i][i][0][0] = 1.
             weights[i+3][i][1][0] = 1.
             weights[i+6][i][0][1] = 1.
             weights[i+9][i][1][1] = 1.
-        self.conv1 = nn.Conv2d(3, 12, 2, 2, padding=0, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, in_channels * 4, 2, 2, padding=0, bias=False)
         self.conv1.weight = nn.Parameter(weights, requires_grad=False)
-        self.conv2 = nn.Conv2d(12, 48, 3, 1, padding=1, bias=False)
+        self.conv2 = BaseConv(in_channels * 4, out_channels, ksize, stride, act=act)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -23,7 +62,7 @@ class FocusV2(nn.Module):
 
 if __name__ == '__main__':
     x = torch.randn(1, 3, 640, 640)
-    focus = FocusV2()
+    focus = FocusV2(3, 48, 3)
     y = focus(x)
     print(y.shape)
     # convert to .onnx via torch.onnx.dynamo_export
