83eaa318560ce3282d74f191172ce0351519162c
HTHloveYDH <hanteikou@gmail.com>
2024-06-27 00:43:31 +0800

    update test

--- Samsung_Exynos_Auto/InstanceNorm2dV2.py
+++ Samsung_Exynos_Auto/InstanceNorm2dV2.py
@@ -3,14 +3,18 @@ import torch.nn as nn
 
 
 class InstanceNorm2dV2(nn.Module):
-    def __init__(self, in_channels:int, momentum=0.1, eps=1e-5, affine=False, track_running_stats=False,
+    def __init__(self, in_channels:int, eps=1e-5, momentum=0.1, affine=False, track_running_stats=False,
                  device=None, dtype=None):
         super(InstanceNorm2dV2, self).__init__()
-        self.run_mean = torch.zeros(1, in_channels, 1, 1, requires_grad=False)
-        self.run_var = torch.zeros(1, in_channels, 1, 1, requires_grad=False)
+        if track_running_stats:
+          self.running_mean = torch.zeros(1, in_channels, 1, 1, requires_grad=False)
+          self.running_var = torch.ones(1, in_channels, 1, 1, requires_grad=False)
+        else:
+          self.run_mean = None
+          self.run_var = None
         self.in_channels = in_channels
-        self.momentum = momentum
         self.eps = eps
+        self.momentum = momentum
         self.affine = affine
         self.track_running_stats = track_running_stats
         self.device = device
@@ -19,22 +23,26 @@ class InstanceNorm2dV2(nn.Module):
             # γ and 𝛽 are learnable parameter vectors of size C (where C is the input size)
             self.gamma = nn.Parameter(torch.ones(1, in_channels, 1, 1), requires_grad=True)
             self.beta = nn.Parameter(torch.ones(1, in_channels, 1, 1), requires_grad=True)
+        else:
+          self.gamma = None
+          self.beta = None
 
     def forward(self, input):
         # bs, c, h, w = input.shape
         if self.track_running_stats and self.training:
             curr_mean = input.mean(dim=(2, 3), keepdim=True)  # (bs, c, 1, 1)
             curr_var = input.var(dim=(2, 3), keepdim=True)  # (bs, c, 1, 1)
-            self.run_mean = curr_mean * self.momentum + self.run_mean * (1.0 - self.momentum)
-            self.run_var = curr_var * self.momentum + self.run_var * (1.0 - self.momentum)
-        output = (input - self.run_mean) / torch.sqrt(self.run_var + self.eps)
+            self.running_mean = curr_mean * self.momentum + self.running_mean * (1.0 - self.momentum)
+            self.running_var = curr_var * self.momentum + self.running_var * (1.0 - self.momentum)
+        output = (input - self.running_mean) / torch.sqrt(self.running_var + self.eps)
         if self.affine:
             output = self.gamma * output + self.beta
         return output
     
 
 if __name__ == '__main__':
-    # train
+    # generate onnx
+    # train mode
     torch_input = torch.randn(1, 3, 640, 640)
     IN = InstanceNorm2dV2(3, affine=True, track_running_stats=True)
     IN.train()
@@ -49,7 +57,7 @@ if __name__ == '__main__':
         input_names = ['input'],   # the model's input names
         output_names = ['output'], # the model's output names
     )
-    # eval
+    # eval mode
     torch_input = torch.randn(1, 3, 640, 640)
     IN = InstanceNorm2dV2(3, affine=True, track_running_stats=True)
     IN.eval()
---
+++ Samsung_Exynos_Auto/InstanceNorm2dV2_test.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from InstanceNorm2dV2 import InstanceNorm2dV2
+    
+
+if __name__ == '__main__':
+    torch_input = torch.randn(1, 2, 2, 2)
+    # my version
+    INV2 = InstanceNorm2dV2(2, affine=False, track_running_stats=True)
+    print('initial running mean of InstanceNorm2dV2: ', INV2.running_mean)
+    print('initial running var of InstanceNorm2dV2: ', INV2.running_var)
+    INV2.train()
+    y_v2 = INV2(torch_input)
+    # pytorch version
+    IN = nn.InstanceNorm2d(2, affine=False, track_running_stats=True)
+    print('initial running mean of InstanceNorm2d: ', IN.running_mean)
+    print('initial running var of InstanceNorm2d: ', IN.running_var)
+    IN.train()
+    y = IN(torch_input)
+    print('updated running mean of InstanceNorm2dV2: ', INV2.running_mean, 'updated running mean of InstanceNorm2d: ', IN.running_mean)
+    print('updated running var of InstanceNorm2dV2: ', INV2.running_var, 'updated running mean of InstanceNorm2d: ', IN.running_var)
+    print(abs(y - y_v2))
