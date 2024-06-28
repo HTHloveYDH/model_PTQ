8467baa1c078100779eb0784ec593d1d95617aff
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-06-29 00:38:29 +0800

    Create replace_in_by_bn.py

---
+++ My_Edge_Device/replace_in_by_bn.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+def replace_in_by_bn(model:nn.Module):
+  i = 0
+  for m in model:
+    i += 1
+    if instance(m, nn.InstanceNorm2d):
+      bn = nn.BatchNorm()
+      bn.running_mean = m.running_mean
+      bn.running_var = m.running_var
+      model[i] = bn
