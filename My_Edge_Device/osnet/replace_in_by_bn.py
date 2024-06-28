1a210e819a379115726c26473b48e88ecbd25345
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-06-29 02:46:58 +0800

    Update replace_in_by_bn.py

--- My_Edge_Device/osnet/replace_in_by_bn.py
+++ My_Edge_Device/osnet/replace_in_by_bn.py
@@ -5,9 +5,14 @@ import torch.nn as nn
 def replace_in_by_bn(model):
     for name, module in model.named_children():
         if isinstance(module, nn.InstanceNorm2d):
-            bn = nn.BatchNorm2d(mm.num_features)
-            bn.running_mean = mm.running_mean
-            bn.running_var = mm.running_var
+            bn = nn.BatchNorm2d(
+                module.num_features, eps=module.eps, momentum=module.momentum, 
+                affine=module.affine, track_running_stats=module.track_running_stats
+            )
+            bn.running_mean = module.running_mean
+            bn.running_var = module.running_var
+            bn.weight = module.weight
+            bn.bias = module.bias
             setattr(model, name, bn)
         elif len(list(module.children())) > 0:
             replace_in_by_bn(module)
