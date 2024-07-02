196280634a8fa5d8bf3f587d663837fc55d3551d
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-02 10:47:12 +0800

    Update BatchNorm2dV3_test.py

--- My_Edge_Device/osnet/BatchNorm2dV3_test.py
+++ My_Edge_Device/osnet/BatchNorm2dV3_test.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
     
     ### dummy training stage
     # my version
-    BNV3 = InstanceNorm2dV3(4, affine=False, track_running_stats=True)
+    BNV3 = BatchNorm2dV3(4, affine=False, track_running_stats=True)
     print('initial running mean of BatchNorm2dV3: ', BNV3.running_mean)
     print('initial running var of BatchNorm2dV3: ', BNV3.running_var)
     BNV3.train()
     y_v3 = BNV3(torch_input)
     # pytorch version
-    BN = nn.InstanceNorm2d(4, affine=False, track_running_stats=True)
+    BN = nn.BatchNorm2d(4, affine=False, track_running_stats=True)
     print('initial running mean of BatchNorm2d: ', BN.running_mean)
     print('initial running var of BatchNorm2d: ', BN.running_var)
     BN.train()
