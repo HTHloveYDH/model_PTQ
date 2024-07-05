b393bea23a4f261e01787589b8c23ca86ec0709e
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-07-06 01:51:39 +0800

    Update load_pretrained_weights_and_replace_Focus_with_FocusV2_in_Pytorch.py

--- My_Edge_Device/yolox/load_pretrained_weights_and_replace_Focus_with_FocusV2_in_Pytorch.py
+++ My_Edge_Device/yolox/load_pretrained_weights_and_replace_Focus_with_FocusV2_in_Pytorch.py
@@ -1,5 +1,7 @@
+######################## option1 begin
+
 # add line 2 in this file to line 4 in YOLOX/tools/export_onnx.py
-from yolox.models.network_blocks import FocusV2
+from yolox.models.network_blocks import Focus, FocusV2
 
 # add line 5 - line 12 in this file to line 13 in YOLOX/tools/export_onnx.py
 def replace_Focus_with_FocusV2(model):
@@ -14,3 +16,30 @@ def replace_Focus_with_FocusV2(model):
 
 # add line 16 in this file to line 78 after 'model.load_state_dict(ckpt)' in the YOLOX/tools/export_onnx.py
 replace_Focus_with_FocusV2(yolox)
+
+######################## option1 end
+
+
+######################## option2 begin
+from yolox.models.network_blocks import Focus, FocusV2
+from yolox.models.yolox import YOLOX
+
+def replace_Focus_with_FocusV2(model):
+    for name, module in model.named_children():
+        if isinstance(module, Focus):
+            focusv2 = FocusV2(in_channels=module.conv.conv.in_channels, out_channels=module.conv.conv.out_channels)
+            focusv2.conv2 = module.conv
+            setattr(model, name, focusv2)
+        elif len(list(module.children())) > 0:
+            replace_Focus_with_FocusV2(module)
+
+
+if __name__ == '__main__':
+    yolox = YOLOX()
+    print(yolox)
+    yolox.eval()
+    print(yolox.head.training)
+    print(yolox.training)
+    print(yolox.head.decode_in_inference)
+    yolox.load_state_dict('path/to/yolox_s.pth')
+    replace_Focus_with_FocusV2(yolox)
